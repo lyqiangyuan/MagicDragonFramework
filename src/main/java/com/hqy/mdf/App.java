b5313522d0c9b79186d1952d64c31ac7e9d7a96a
lyqiangyuan <445675226@qq.com>
2025-04-23 14:26:48 +0800

    1

---
+++ src/main/java/com/hqy/mdf/App.java
@@ -0,0 +1,13 @@
+package com.hqy.mdf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
